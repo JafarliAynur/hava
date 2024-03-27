93a591b95913ade606eba76e7c249506a49ef79f
AynurJafarli <jafarliaynur13@gmail.com>
2024-03-27 11:58:48 +0400

    uteryergbej

--- app.js
+++ app.js
@@ -1,24 +1,44 @@
-
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
-const apiKey = 'YOUR_API_KEY';
-
-
-async function getWeather(city) {
-    const response = await fetch(`${apiUrl}?q=${city}&appid=${apiKey}&units=metric`);
-    const data = await response.json();
-    return data;
-}
-
-
-async function updateWeather() {
-    const city = document.getElementById('cityInput').value;
-    const weatherData = await getWeather(city);
-    
-  
-    document.getElementById('cityName').textContent = weatherData.name;
-    document.getElementById('temperature').textContent = weatherData.main.temp + '°C';
-    document.getElementById('description').textContent = weatherData.weather[0].description;
-}
-
-
-document.getElementById('updateButton').addEventListener('click', updateWeather);
+const hours = document.querySelector("#hours");
+const minutes = document.querySelector("#minutes");
+const seconds = document.querySelector("#seconds");
+const weekday = document.querySelector("#weekday");
+const weekindays = document.querySelector("#weekindays");
+const months = document.querySelector("#months");
+let daysInWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let month = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+setInterval(() => {
+  const time = new Date();
+  hours.innerHTML =
+    time.getHours() < 10 ? "0" + time.getHours() : time.getHours();
+  if (time.getMinutes() < 10) {
+    minutes.innerHTML = "0" + time.getMinutes();
+  } else {
+    minutes.innerHTML = time.getMinutes();
+  }
+  seconds.innerHTML =
+    time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
+  weekday.innerHTML = ", " + time.getDate();
+  weekindays.innerHTML = daysInWeek[time.getDay()].slice(0, 3);
+  months.innerHTML = ", " + month[time.getMonth()];
+}, 1000);
